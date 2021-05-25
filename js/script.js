904e0eab70362141794a694f525e42873712d541
anitkumarsingh <anikumarsingh99@gmail.com>
2021-05-25 21:21:55 +0530

    events

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-var scores, dices, activePlayer, roundScores;
+var scores, activePlayer, roundScores;
 scores = [0, 0];
-dices = Math.floor(Math.random() * 6) + 1;
 activePlayer = 0;
 roundScores = 1;
 
-document.querySelector('#current--' + activePlayer).textContent = dices;
+// document.querySelector('#current--' + activePlayer).textContent = dices;
 var x = document.querySelector('#score--' + activePlayer).textContent;
 
 // hide dices at the begining
 document.querySelector('.dice').style.display = 'none';
 console.log(x);
+
+document.querySelector('.btn--roll').addEventListener('click', function () {
+  // 1 Generate random number
+  var dices = Math.floor(Math.random() * 6) + 1;
+
+  // 2 change image
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = '/img/dice-' + dices + '.png';
+});
